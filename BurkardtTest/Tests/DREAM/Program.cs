7b91d110060909241c891d33014813c44a3ef958
philstopford <phil.stopford@gmail.com>
2024-01-22 21:19:20 -0600

    More unit test WIP

---
+++ BurkardtTest/Tests/DREAM/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Burkhardt_Tests.DREAMTest;
+
+internal static class Program
+{
+    private static void test()
+    {
+        Problem0.test();
+        Problem1.test();
+        Problem1c.test();
+        Problem2.test();
+    }
+}
